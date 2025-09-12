5b9d98eea7a017add6e4d04477cfd778f0c02da7
SimonKurono <simonkurono@gmail.com>
2025-09-12 09:46:50 -0700

    Added backtesting to moving average strategy

--- helpers/moving_average_analysis.py
+++ helpers/moving_average_analysis.py
@@ -31,3 +31,31 @@ def moving_average_strategy(data):
     data['Signal'][data['MA_20'] < data['MA_50']] = -1
     data['Position'] = data['Signal'].diff()
     return data
+
+def backtest_strategy(data, initial_capital=10000):
+    """
+    Backtest the moving average strategy.
+    
+    Parameters:
+    data (pd.DataFrame): DataFrame with 'Close' and 'Position' columns.
+    initial_capital (float): Starting capital for backtesting.
+    
+    Returns:
+    pd.DataFrame: DataFrame with portfolio value over time.
+    """
+    data['Portfolio Value'] = initial_capital
+    position = 0
+    for i in range(1, len(data)):
+        if data['Position'].iloc[i] == 1:  # Buy signal
+            position = data['Portfolio Value'].iloc[i-1] / data['Close'].iloc[i]
+            data['Portfolio Value'].iloc[i] = 0
+        elif data['Position'].iloc[i] == -1 and position > 0:  # Sell signal
+            data['Portfolio Value'].iloc[i] = position * data['Close'].iloc[i]
+            position = 0
+        else:
+            data['Portfolio Value'].iloc[i] = data['Portfolio Value'].iloc[i-1]
+    
+    if position > 0:  # If holding a position at the end, sell it
+        data['Portfolio Value'].iloc[-1] = position * data['Close'].iloc[-1]
+    
+    return data
