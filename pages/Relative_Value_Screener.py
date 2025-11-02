0b98efa557be5b9ca336a591ff025d997300ab30
SimonKurono <simonkurono@gmail.com>
2025-11-02 10:59:36 -0800

    Fixed spelling mistakes

--- pages/Relative_Value_Screener.py
+++ pages/Relative_Value_Screener.py
@@ -4,4 +4,4 @@ import utils.ui as ui
 
 ui.render_sidebar()
 st.header("Coming Soon")
-st.write("TODO: add treasury stats adn fixed income downloadable metrics")
+st.write("TODO: add treasury stats and fixed income downloadable metrics")
